537cd00cddd8c2401be6eeb886b4c3df9e113b63
chrisalexander <chris@chris-alexander.co.uk>
2012-11-22 12:22:10 +0000

    Log levels

--- feedback/LogLevels.py
+++ feedback/LogLevels.py
@@ -5,3 +5,17 @@ class LogLevels(object):
     INFO = 3
     WARN = 4
     ERROR = 5
+    
+    def getLevel(self, level):
+        if level == 1:
+            return "TRACE"
+        elif level == 2:
+            return "DEBUG"
+        elif level == 3:
+            return "INFO"
+        elif level == 4:
+            return "WARN"
+        elif level == 5:
+            return "ERROR"
+        else:
+            return "LOG"
